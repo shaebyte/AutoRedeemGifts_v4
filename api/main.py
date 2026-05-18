5c910d7a04c54788a5e9c6332e763faa3dc668ce
Shaebyte <shaebyte@gmail.com>
2026-05-18 18:07:49 +0200

    auth call change

--- api/main.py
+++ api/main.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv(Path(__file__).resolve().parent.parent / ".env")
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from .auth import router as auth_router
